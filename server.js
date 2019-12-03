307675ff2f1b206ab39e1da8419ff9207ab3c5af
NIPONNew6030211015 <s6030211015@phuket.psu.ac.th>
2019-12-03 18:39:01 +0700

    finish

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 // call the packages we need
 // #1 Add express package to the app
-
+var express = require('express');
 // ===============================
 
 var app = express();   
 var cors = require('cors');       
 
 // #2 Add body-parser package to the app
-
+var bodyParser = require('body-parser');
 // ===============================
 
 
@@ -18,7 +18,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // #3 Serve static content in folder frontend
-
+app.get('/', function (req, res) {
+    res.render('frontend/index');
+});
 // ===============================
 
 
@@ -31,9 +33,35 @@ var router = express.Router();              // get an instance of the express Ro
 var products = require('./api');
 router.get('/products', products.getAllProducts);
 router.get('/products/:pid', products.getProductById);
+router.post('/products/:pid', products.postProductById);
+router.update('/products/:pid', products.updateProductById);
+router.delete('/products/:pid', products.deleteProductById);
 
 // #4 Complete the routing for POST, PUT, DELETE
+app.get('/api/products/:id', function(req, res){
+    var id = req.params.id;
+  Product.find({"_id":id},function(err, products){
+    if (err) res.status(500).json(err);
+    res.json(products);
+  });
+});
 
+app.put('/api/products/:id', function (req, res){
+    var id = req.params.id;
+    var updateproduct = req.body;
+    Product.findByIdAndUpdate(id, updateproduct, function(err){
+      if (err) res.status(500).json(err);      
+      res.json({status: "Update a product"});
+    })
+});
+
+app.delete('/api/products/:id', function (req, res){
+    var id = req.params.id;
+    Product.findByIdAndRemove(id, function(err){
+      if (err) res.status(500).json(err);      
+      res.json({status: "product deleted  "});
+    })
+});
 // ===============================
 
 
@@ -44,4 +72,4 @@ app.use('/api', cors(), router);
 // #10 Start the server
 
 // ===============================
-console.log('Magic happens on http://localhost:' + port);
+console.log('Magic happens on http://localhost:' + 8080);
